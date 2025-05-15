c48e80375b831ce01b96e1384eaaa0570ac33a5b
Moe Pwint <moezatmoepwint@gmail.com>
2025-05-16 00:52:55 +0700

    Initiate basic html, css, js setups and webpack.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+
+import { greeting } from "./greeting.js";
+
+console.log(greeting);
+
+
+import odinImage from "./images/odin.png";
+
+const image = document.createElement("img");
+        image.src = odinImage;
+        
+        document.body.appendChild(image);
